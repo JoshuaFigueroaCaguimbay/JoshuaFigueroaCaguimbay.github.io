08ee466ac7a93391e7267cc1d1032cf21e5bae32
JoshuaFigueroaCaguimbay <joshua.figueroacaguimbay@gmail.com>
2024-07-28 16:01:27 -0400

    Fixed mobile scaling

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,13 +4,21 @@ import resume from "../assets/resume.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-
+import { useState, useEffect } from "react";
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.mjs',
     import.meta.url,
 ).toString();
 
-function Resume(){
+function Resume(){ 
+    const [scale, setScale] = useState(0.04 * Math.sqrt(window.innerWidth) - 0.1);   
+     
+    useEffect(() => { 
+        window.addEventListener("resize", () => setScale(0.04 * Math.sqrt(window.innerWidth) - 0.1));  
+        return () => 
+            window.removeEventListener("resize", () => setScale(0.04 * Math.sqrt(window.innerWidth) - 0.1));
+    }, []);
+
     return(
         <div>
             <Container fluid className="resume-section">
@@ -22,7 +30,7 @@ function Resume(){
                             alert(`ERROR: Document failed to load ${error.message}`)
                         }
                     >
-                        <Page pageNumber={1} scale={1.7} />
+                        <Page pageNumber={1} scale={scale} />
                     </Document>
                 </Row>
             </Container>
